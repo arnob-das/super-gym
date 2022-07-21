0149661f9ae989724cd544add457914e369f2f42
arnob-das <arnob.cse.20210104148@aust.edu>
2022-07-22 01:53:40 +0600

    programs section done

---
+++ src/components/Programs/Programs.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Programs.css';
+import { programsData } from '../../data/programsData';
+import rightArrow from '../../assets/rightArrow.png'
+
+const Programs = () => {
+    return (
+        <div className="programs" id="programs">
+            <div className="programs-header">
+                <span className="stroke-text">Explore Our</span>
+                <span>Progress</span>
+                <span className="stroke-text">To Shape You</span>
+            </div>
+            <div className="programs-categories">
+                {programsData.map(program => (
+                    <div className="category">
+                        {program.image}
+                        <span>{program.heading}</span>
+                        <span>{program.details}</span>
+                        <div className="join-now">
+                            <span>Join Now</span>
+                            <img src={rightArrow} alt="" />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Programs;
